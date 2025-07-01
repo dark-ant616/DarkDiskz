697f4556485c3dfd47016fc123e5e301b722616b
dark-ant616 <antz23616@gmail.com>
2025-07-01 05:49:17 -0400

    Add files via upload

---
+++ install_deps.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+sudo apt update
+sudo apt install -y \
+  python3 \
+  python3-gi \
+  python3-gi-cairo \
+  gir1.2-gtk-4.0 \
+  gir1.2-adw-1 \
+  bcache-tools \
+  mdadm \
+  smartmontools \
+  wipefs \
+  lsb-release \
+  lshw \
+  lsblk \
+  lspci \
+  udev \
+  xterm
+
